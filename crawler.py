9e9185b733f341beece913f2f4e8143f605a3a4a
Le Thanh Son <thanhson@MacBook-Pro-cua-Son.local>
2021-11-27 16:41:25 +0700

    Fixed some bugs, usable now

--- crawler.py
+++ crawler.py
@@ -10,32 +10,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def crawl_data(driver, page_threshold=2):
+def crawl_data(driver, page_threshold=2, country_name_list=None):
     # page_threshold = 2
+    if country_name_list is None:
+        country_name_list = []
     page_count = 1
-    for city_name in city_name_list:
+    for country_name in country_name_list:
         # Find the city
-        driver.find_element(By.XPATH, '//*[@id="component_1"]/div/div/form/input[1]').send_keys(city_name)
+        write_to_csv(country_name+"_data.csv", "country_name", "place_name", "review_header", "review", "written_date")
+        driver.find_element(By.XPATH, '//*[@id="component_1"]/div/div/form/input[1]').send_keys(country_name)
         sleep(1)
         driver.find_element(By.XPATH, '//*[@id="component_1"]/div/div/form/input[1]').send_keys(Keys.ARROW_DOWN)
         sleep(1)
         driver.find_element(By.XPATH, '//*[@id="component_1"]/div/div/form/input[1]').send_keys(Keys.ENTER)
-        sleep(1)
+        sleep(3)
         driver.find_element(By.XPATH,
-                            '//*[@id="lithium-root"]/main/span/div/div[3]/div/div/div/span/div/div/div[2]/div/div/section[4]/div/span/div/div[3]/div/a').click()
+                            '//*[@id="lithium-root"]/main/span/div/div[3]/div/div/div/span/div/div/div[2]/div/div/section[3]/div/span/div/div[3]/div/a').click()
         sleep(1)
+        # //*[@id="lithium-root"]/main/span/div/div[3]/div/div/div/span/div/div[2]/div[2]/div/div/section[2]/div/span/div/article/div[2]/header/div/div/a[1]
+        # //*[@id="lithium-root"]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1
         driver.switch_to.window(driver.window_handles[-1])
-
         sleep(1)
         # Click on each link
         while page_count <= page_threshold:
             ad_counter = 1  # To avoid adplaceholder
-            for i in range(2, 3):
+            for i in range(2, 38):
                 if ad_counter == 5:
                     ad_counter = 1
                     continue
-
+                # //*[@id="lithium-root"]/main/span/div/div[3]/div/div/div/span/div/div[2]/div[2]/div/div/section[38]
                 # tabs = driver.window_handles
                 # print(len(tabs))
                 # //*[@id="lithium-root"]/main/span/div/div[3]/div/div/div/span/div/div[2]/div[2]/div/div/section[3]/div/span/div/article/div[2]/header/div/div/a[1]
@@ -47,7 +53,7 @@ def crawl_data(driver, page_threshold=2):
                                                                                 '2]/header/div/div/a[1]').click()
                     driver.switch_to.window(driver.window_handles[-1])
                     sleep(4)
-                    get_review(driver, 2)
+                    get_review(driver, 30, country_name)
                     # driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.COMMAND + "w")
                     driver.close()
                     driver.switch_to.window(driver.window_handles[-1])
@@ -68,16 +74,21 @@ def crawl_data(driver, page_threshold=2):
 
 
 # This section is to get the reviews on a place
-def get_review(driver, page_threshold=2):
+def get_review(driver, page_threshold=2, country=""):
     page_counter = 1
     name_place = driver.find_element(By.XPATH,
                                      '//*[@id="lithium-root"]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1').text
-    region_place = driver.find_element(By.XPATH,
-                                       '/html/body/div/main/div[1]/div[2]/div[2]/div/div/span/section[5]/div/div/div[2]/div[1]/span/div/div/div[1]/button/span').text
+    driver.implicitly_wait(1)
+    # try:
+    #     region_place = driver.find_element(By.XPATH,
+    #                                        '//*[@id="tab-data-WebPresentation_PoiLocationSectionGroup"]/div/div/div[2]/div[1]/span/div/div/div[1]/button/span').text
+    # except:
+    #     region_place =""
 
-    file_name = "~/data/" + name_place + " " + region_place + " " + "data.csv"
+    file_name = country + "_data.csv"
     # print(name_place, region_place)
     while page_counter <= page_threshold:
+        sleep(3)
         # Get the reviews in one page
         for i in range(1, 11):
             try:
@@ -99,19 +110,30 @@ def get_review(driver, page_threshold=2):
                 header = ""
 
             # print(header, review, written, '\n')
-            write_to_csv(file_name, name_place, region_place, header, review, written)
+            write_to_csv(file_name, country, name_place, header, review, written)
         # Click the next page
 
-        driver.find_element(By.XPATH,
-                            '//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[11]/div[1]/div/div[1]/div[2]/div/a').click()
-        page_counter += 1
+        next_button = driver.find_element(By.XPATH,
+                            '//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[11]/div[1]/div/div[1]/div[2]/div/a')
+        if next_button:
+            driver.implicitly_wait(5)
+
+            # WebDriverWait(driver, 10).until(
+            #     EC.element_to_be_clickable((By.XPATH, '//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[11]/div[1]/div/div[1]/div[2]/div/a'))).click()
+            next_button.click()
+            page_counter +=1
+            driver.implicitly_wait(5)
+        else:
+            break
+        # driver.find_element(By.XPATH,
+        #                     '//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[11]/div[1]/div/div[1]/div[2]/div/a').click()
+        # page_counter += 1
 
 
-def write_to_csv(filename, place_name, region_place, review_header, review, date_review):
-    with open(filename, mode='w') as file:
+def write_to_csv(filename, country, place_name, review_header, review, date_review):
+    with open(filename, 'a', newline='') as file:
         writer_obj = writer(file)
-        writer_obj.writerow([place_name, region_place, review_header, review, date_review])
-        file.close()
+        writer_obj.writerow([country,place_name, review_header, review, date_review])
 
 
 options = Options()
@@ -119,8 +141,10 @@ options = Options()
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-city_name_list = ['Paris']
+country_list = ['France']
 
 driver.get('https://www.tripadvisor.com/Attractions')
 driver.implicitly_wait(3)
-crawl_data(driver, 10)
+crawl_data(driver, 10, country_list)
+
+
